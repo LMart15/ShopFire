7993401d30366a18fff6e76bed2929c3075ccc41
Lawrence Martin <lawrence.martin@cantire.com>
2017-02-21 18:55:43 -0500

    initial commit

---
+++ Pods/Target Support Files/Pods-MAPD124-W2017-MIDTERM/Pods-MAPD124-W2017-MIDTERM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MAPD124_W2017_MIDTERM : NSObject
+@end
+@implementation PodsDummy_Pods_MAPD124_W2017_MIDTERM
+@end
---
+++ Pods/Target Support Files/Pods-MAPD124-W2017-MIDTERM/Pods-MAPD124-W2017-MIDTERM-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MAPD124_W2017_MIDTERMVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MAPD124_W2017_MIDTERMVersionString[];
+
